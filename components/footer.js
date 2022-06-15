c319c3fc44dd4c24adc5eae9f64ccb7b4d0c4b05
Karan Joshi <58625638+pythondevloper007@users.noreply.github.com>
2022-06-15 09:45:05 +0530

    Base Structure Added

---
+++ components/footer.js
@@ -0,0 +1,126 @@
+// import "../styles/footer";
+
+function footer() {
+  return `<hr />
+    <div id="footer">
+      <div class="rows">
+        <div class="fcol">
+          <h2 class="heading">GET TO KNOW US</h2>
+          <div class="app-link">
+            <a class="link" href="https://www.dotandkey.com/pages/about-us">ABOUT US</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/pages/find-us">Contact us</a
+            ><br />
+
+            <a class="link" href="https://www.dotandkey.com/pages/csr">Earthcare</a><br />
+            <a class="link" href="https://divas.dotandkey.com/">affilate program</a><br />
+            <a class="link" href="https://www.dotandkey.com/pages/terms-conditions"
+              >TERMS & CONDITION</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/pages/refunds-cancellation"
+              >REFUND & CANCELLATION</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/pages/privacy-policy"
+              >PRIVACY POLICY</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/pages/orders-shipping"
+              >ORDER & SHIPPING</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/pages/payment-terms"
+              >PAYMENT TERMS</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/policies/terms-of-service"
+              >TERMS OF SERVICE</a
+            ><br />
+            <a class="link" href="https://www.dotandkey.com/policies/refund-policy"
+              >REFUND POLICY</a
+            ><br />
+          </div>
+        </div>
+        <div class="fcol">
+          <h2>ALSO AVAILABLE ON</h2>
+          <div class="app-link">
+            <a
+              class="link" href="https://www.nykaa.com/brands/dot-key/c/7675?root=search,brand_menu,brand_list,Dot%20&%20Key&sourcepage=home&rtqu=Dot%20%26%20Key"
+              >Nykaa</a
+            ><br />
+            <a
+              class="link" href="https://www.amazon.in/stores/node/19401367031?_encoding=UTF8&field-lbr_brands_browse-bin=Dot%20%26%20Key&ref_=bl_dp_s_web_19401367031"
+              >Amazon</a
+            ><br />
+
+            <a
+              class="link" href="https://www.flipkart.com/search?q=dot%20and%20key&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+              >Flipkart</a
+            ><br />
+            <a class="link" href="https://www.myntra.com/dot-and-key?src=bc">Myntra</a><br />
+          </div>
+        </div>
+        <div class="fcol">
+          <h2>STAY UPDATED</h2>
+          <div class="news-letter">
+            <input class="input-box" type="email" id="email" placeholder="example@example.com" />
+            <button class="submit">Submit</button>
+          </div>
+          <div class="follow-us">
+            <p class="follow-line">FOLLOW US</p>
+            <a target="_blank" class="link" href="https://business.facebook.com/dotandkey"
+              ><svg
+                aria-hidden="true"
+                focusable="false"
+                role="presentation"
+                class="icon icon-facebook"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="#444"
+                  d="M18.56 31.36V17.28h4.48l.64-5.12h-5.12v-3.2c0-1.28.64-2.56 2.56-2.56h2.56V1.28H19.2c-3.84 0-7.04 2.56-7.04 7.04v3.84H7.68v5.12h4.48v14.08h6.4z"
+                ></path>
+                <path fill="#444" d=""></path></svg></a
+            ><br />
+            <a
+              target="_blank"
+              class="link" href="https://www.instagram.com/dotandkey.skincare/"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                role="presentation"
+                class="icon icon-instagram"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="#444"
+                  d="M16 3.094c4.206 0 4.7.019 6.363.094 1.538.069 2.369.325 2.925.544.738.287 1.262.625 1.813 1.175s.894 1.075 1.175 1.813c.212.556.475 1.387.544 2.925.075 1.662.094 2.156.094 6.363s-.019 4.7-.094 6.363c-.069 1.538-.325 2.369-.544 2.925-.288.738-.625 1.262-1.175 1.813s-1.075.894-1.813 1.175c-.556.212-1.387.475-2.925.544-1.663.075-2.156.094-6.363.094s-4.7-.019-6.363-.094c-1.537-.069-2.369-.325-2.925-.544-.737-.288-1.263-.625-1.813-1.175s-.894-1.075-1.175-1.813c-.212-.556-.475-1.387-.544-2.925-.075-1.663-.094-2.156-.094-6.363s.019-4.7.094-6.363c.069-1.537.325-2.369.544-2.925.287-.737.625-1.263 1.175-1.813s1.075-.894 1.813-1.175c.556-.212 1.388-.475 2.925-.544 1.662-.081 2.156-.094 6.363-.094zm0-2.838c-4.275 0-4.813.019-6.494.094-1.675.075-2.819.344-3.819.731-1.037.4-1.913.944-2.788 1.819S1.486 4.656 1.08 5.688c-.387 1-.656 2.144-.731 3.825-.075 1.675-.094 2.213-.094 6.488s.019 4.813.094 6.494c.075 1.675.344 2.819.731 3.825.4 1.038.944 1.913 1.819 2.788s1.756 1.413 2.788 1.819c1 .387 2.144.656 3.825.731s2.213.094 6.494.094 4.813-.019 6.494-.094c1.675-.075 2.819-.344 3.825-.731 1.038-.4 1.913-.944 2.788-1.819s1.413-1.756 1.819-2.788c.387-1 .656-2.144.731-3.825s.094-2.212.094-6.494-.019-4.813-.094-6.494c-.075-1.675-.344-2.819-.731-3.825-.4-1.038-.944-1.913-1.819-2.788s-1.756-1.413-2.788-1.819c-1-.387-2.144-.656-3.825-.731C20.812.275 20.275.256 16 .256z"
+                ></path>
+                <path
+                  fill="#444"
+                  d="M16 7.912a8.088 8.088 0 0 0 0 16.175c4.463 0 8.087-3.625 8.087-8.088s-3.625-8.088-8.088-8.088zm0 13.338a5.25 5.25 0 1 1 0-10.5 5.25 5.25 0 1 1 0 10.5zM26.294 7.594a1.887 1.887 0 1 1-3.774.002 1.887 1.887 0 0 1 3.774-.003z"
+                ></path>
+              </svg> </a
+            ><br />
+            <a
+              target="_blank"
+              class="link" href="https://www.youtube.com/channel/UCD7U0EUBYg9YlL-zVZjkjfA"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                role="presentation"
+                class="icon icon-youtube"
+                viewBox="0 0 21 20"
+              >
+                <path
+                  fill="#444"
+                  d="M-.196 15.803q0 1.23.812 2.092t1.977.861h14.946q1.165 0 1.977-.861t.812-2.092V3.909q0-1.23-.82-2.116T17.539.907H2.593q-1.148 0-1.969.886t-.82 2.116v11.894zm7.465-2.149V6.058q0-.115.066-.18.049-.016.082-.016l.082.016 7.153 3.806q.066.066.066.164 0 .066-.066.131l-7.153 3.806q-.033.033-.066.033-.066 0-.098-.033-.066-.066-.066-.131z"
+                ></path>
+              </svg> </a
+            ><br />
+          </div>
+          <div class="copyright">© 2022 Dot &amp; Key</div>
+        </div>
+      </div>
+    </div>`;
+}
+
+export default footer;
