229e2a0dc7583d662f208aaf6286adf363f4a34b
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2022-06-16 17:12:28 +0530

    serum page added

---
+++ scripts/serum.js
@@ -0,0 +1,263 @@
+let data=[
+    {p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Untitled-1_e13fe0e4-6313-45f1-b5f6-fe829a4c5dec_360x.jpg?v=1650465566",
+    p_name:"Vitamin C Serum with Blood Orange | For Skin Glow | Fades Pigmentation & Dark Spots| Combination, Oily, Dry Skin | Women and Men",
+    p_price:"Rs: 665.00",
+    p_dprice:632.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1_3057d434-6349-42b8-aefe-1673c8d24265_360x.jpg?v=1649327316",
+    p_name:"Niacinamide Face Serum With Zinc & Antioxidants| Treats Acne, Dark Spots & Acne Scars| For Oily, Sensitive ,Acne Prone Skin| Women & Men",
+    p_price:"Rs: 665.00",
+    p_dprice: 598.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-7_2_360x.jpg?v=1646476789",
+    p_name:"RETINOL TIME REVERSE FACE SERUM",
+    p_price:"Rs: 795.00",
+    p_dprice:715.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1_4_7a306ce8-6ac4-4704-9284-aa5264e20546_360x.jpg?v=1646476826",
+    p_name:"HYALURONIC + CERAMIDE HYDRATING FACE SERUM",
+    p_price:"Rs: 665.00",
+    p_dprice:598.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1_32f28fd1-371e-4840-b4a1-216f31a43318_360x.jpg?v=1649328433",
+    p_name:"Vitamin C++ Glow Face Serum With Hyaluronic & Kakadu Plum| For Skin Glow| Fades Pigmentation & Dark Spots| Oily,Dry Skin| Women & Men",
+    p_price:"Rs: 795.00",
+    p_dprice:715.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-6_3_360x.jpg?v=1646477545",
+    p_name:"AHA + 2% BHA EXFOLIATING GLOW HYDRO PEEL",
+    p_price:"Rs: 595.00",
+    p_dprice:565.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+    
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-2_3_360x.jpg?v=1646476857",
+    p_name:"Alpha Arbutin + Azelaic Biphasic RADIANCE Serum",
+    p_price:"Rs: 545.00",
+    p_dprice:518.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-4_4_763d27a2-de17-476f-9058-0891cd25b094_360x.jpg?v=1646476808",
+    p_name:"PHA + Bakuchiol Daily Peel Serum",
+    p_price:"Rs: 995.00",
+    p_dprice:796.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-6_2_ff47654b-ba0b-43c5-ac01-7888e451d4e7_360x.jpg?v=1646476750",
+    p_name:"Salicylic Face Serum With Tea Tree Oil & Zinc | Treats Acne, Blemishes & Acne Scars| Oily, Acne Prone Skin| Women and Men",
+    p_price:"Rs: 665.00",
+    p_dprice:598.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/1_2_360x.jpg?v=1644862670",
+    p_name:"Rosehip Glow Elixir Face Oil",
+    p_price:"Rs: 645.00",
+    p_dprice:516.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-3_4_ae587533-265c-4e47-84bf-81c14bbc6539_360x.jpg?v=1646476775",
+    p_name:"AHA + 2% BHA GLOW ACCELERATOR PEEL",
+    p_price:"Rs: 695.00",
+    p_dprice:556.00,
+    star:"https://image.shutterstock.com/image-vector/four-golden-star-rating-illustration-260nw-1442679176.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/1_1_360x.jpg?v=1644916657",
+    p_name:"Avocado + Superfoods Nourishing Face Oil",
+    p_price:"Rs: 645.00",
+    p_dprice:516.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/SkinAwakening_360x.jpg?v=1610017602",
+    p_name:"SKIN AWAKENING MOISTURIZING SERUM",
+    p_price:"Rs: 795.00",
+    p_dprice:636.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+
+},
+{
+    p_image:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/2_11_360x.webp?v=1644493946",
+    p_name:"AGE DEFENSE + GLOW NIGHT SERUM",
+    p_price:"Rs: 595.00",
+    p_dprice:476.00,
+    star:"https://previews.123rf.com/images/barks/barks1712/barks171200370/91718901-five-star-rating-icon-3-.jpg",
+
+
+},
+]
+
+
+document.querySelector("#sort").addEventListener("change",handleName)
+
+function handleName(){
+var selected=document.querySelector('#sort').value
+
+if(selected=="asc"){
+
+    data.sort(function(a,b){
+        if(a.p_name>b.p_name){
+            return 1
+        }
+        if(a.p_name<b.p_name){
+            return -1
+        }
+        return 0
+    })
+
+    displayData(data);
+
+}
+if(selected=="dsc"){
+
+    data.sort(function(a,b){
+        if(a.p_name>b.p_name){
+            return -1
+        }
+        if(a.p_name<b.p_name){
+            return 1
+        }
+        return 0
+    })
+ 
+    displayData(data);
+   
+
+}
+   
+}
+
+document.querySelector("#sort").addEventListener("change",handlePrice)
+
+function handlePrice(){
+
+    var price=document.querySelector("#sort").value
+
+    // console.log(price);
+    if(price=="HTL"){
+
+        data.sort(function (a,b){
+
+            return  Number(b.p_dprice)-Number(a.p_dprice)
+           
+        })
+        displayData(data);
+        
+    }
+    if(price=="LTH"){
+
+        data.sort(function (a,b){
+
+            let diff=Number(a.p_dprice)-Number(b.p_dprice)
+            return diff
+           
+        })
+        displayData(data);
+
+    }
+    
+
+   
+
+}
+
+
+
+displayData(data)
+
+
+function displayData(data){
+    document.querySelector("#k_cart").innerHTML=null
+    
+
+
+data.forEach(function(el){
+ 
+
+    let box=document.createElement("div");
+    box.setAttribute("id","k_box")
+
+    let img_p=document.createElement("img")
+    img_p.setAttribute("id","k_img")
+    img_p.src=el.p_image;
+
+    let r_img=document.createElement("img")
+    r_img.setAttribute("id","rate")
+    r_img.src=el.star
+
+    let name_p=document.createElement("p")
+    name_p.setAttribute("id","k_name")
+    name_p.innerText=el.p_name;
+    name_p.style.color="grey"
+    name_p.style.textAlign="center"
+
+    let price_p=document.createElement("h3")
+    price_p.setAttribute("id","k_price")
+    price_p.innerText=el.p_price;
+    
+
+
+    let dprice_p=document.createElement("h3")
+    dprice_p.setAttribute("id","k_dprice")
+    dprice_p.innerText="RS:"+el.p_dprice;
+    
+
+   
+
+    let btn=document.createElement("button")
+    btn.innerText="Add to Cart"
+    btn.setAttribute("id","add_cart")
+
+  
+
+   
+
+
+    box.append(img_p,r_img,name_p,price_p,dprice_p,btn)
+
+    // box.addEventListener("click",myClick)
+
+    // function myClick(){
+    //     // window.location.href="product.html"
+    // }
+
+
+     document.querySelector("#k_cart").append(box)
+
+  
+
+
+
+
+})
+
+
+
+}
